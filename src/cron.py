51b33e7c23512abf67dc1ecf543455e4e144b8c5
ageeye <5332429+ageeye@users.noreply.github.com>
2023-10-03 16:33:53 +0200

    Add debug flag

--- src/cron.py
+++ src/cron.py
@@ -1,45 +1,47 @@
 import time
 from maxcert import Project, Route, Environment, AcmeChallenge
 
-print("cron started")
+if not Environment.get('MAXCERT_MODE')=='debug':
 
-# get projects
-base = Environment.get('MAXCERT_PROJECT')
-projects = Project.init()
-projects.remove(base)
+    print("cron started")
 
-# set email
-if Environment.has('MAXCERT_MAIL'):
-    mail = Environment.get('MAXCERT_MAIL')
-    with open('/bot/certbot.ini', 'a') as fd:
-        fd.write(f'email = {mail}\n')
+    # get projects
+    base = Environment.get('MAXCERT_PROJECT')
+    projects = Project.init()
+    projects.remove(base)
 
-# get routes
-Route.cleanHostFiles()
-for project in projects:
-    print('switch to project', project)
-    with Project.select(project):
-        for route in Route.fetch():
-            data = route.as_dict()
-            name = route.name()
-            host = data['spec']['host']
-            Route(project, name, host)
-            print('-', name, host)
-        Route.writeHostFile(project)
+    # set email
+    if Environment.has('MAXCERT_MAIL'):
+        mail = Environment.get('MAXCERT_MAIL')
+        with open('/bot/certbot.ini', 'a') as fd:
+            fd.write(f'email = {mail}\n')
+
+    # get routes
+    Route.cleanHostFiles()
+    for project in projects:
+        print('switch to project', project)
+        with Project.select(project):
+            for route in Route.fetch():
+                data = route.as_dict()
+                name = route.name()
+                host = data['spec']['host']
+                Route(project, name, host)
+                print('-', name, host)
+            Route.writeHostFile(project)
 
 
-for project in projects:
-    print('switch to project', project)
-    for route in Route.all(project):
-        route.setTmpHost()  
-        
-    acme = AcmeChallenge(base, project)
-    acme.start()
-    acme.getcert()
-    acme.cleanup()
-        
-    for route in Route.all(project):
-        route.restoreHost()
+    for project in projects:
+        print('switch to project', project)
+        for route in Route.all(project):
+            route.setTmpHost()  
+            
+        acme = AcmeChallenge(base, project)
+        acme.start()
+        acme.getcert()
+        acme.cleanup()
+            
+        for route in Route.all(project):
+            route.restoreHost()
 
 while Environment.get('MAXCERT_CRON')=='false':
     time.sleep(60 * 60)
